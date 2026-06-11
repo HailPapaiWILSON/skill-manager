df466ed1a6c74e1ec71f432b989db90f99613576
Chappie Projects <projetocolab53@gmail.com>
2026-06-11 09:10:58 -0300

    Add TypeScript types to team routes

--- apps/backend/src/routes/teams.routes.ts
+++ apps/backend/src/routes/teams.routes.ts
@@ -15,18 +15,33 @@ router.get("/", async (_req: any, res: any) => {
   res.json(teams);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req: any, res: any) => {
   const team = await getTeamById(Number(req.params.id));
 
   if (!team) {
     return res.status(404).json({ error: "Equipe nao encontrado" });
   }
 
-  return.json(team);
+  res.json(team);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req: any, res: any) => {
   const team = await createTeam(req.body);
 
   res.status(201).json(team);
 });
+
+router.put("/:id", async (res: any, req: any) => {
+  const team = await updateTeam(Number(req.params.id), req.body);
+
+  res.json(team);
+});
+
+
+router.delete("/:id", async (req: any, res: any) => {
+  await deleteTeam(Number(req.params.id));
+
+  res.sendStatus(204);
+})
+
+export default router;
