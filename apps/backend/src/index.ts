7f04b389f855b4151e1a0975152a03b80a241e7e
Chappie Projects <projetocolab53@gmail.com>
2026-06-11 09:11:16 -0300

    Add teams router to the backend application

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRouter from "./routes/users.routes.js";
+import teamsRouter from "./routes/teams.routes.js"
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/usuarios", usersRouter);
+app.use("/equipes", teamsRouter);
 
 app.listen(3000);
