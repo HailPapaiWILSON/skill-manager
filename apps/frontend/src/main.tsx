e185c5663f50ff312b0f2685aa63919ffc78f950
Chappie Projects <projetocolab53@gmail.com>
2026-06-16 09:52:27 -0300

    Simplify routing structure in main.tsx
    Refactor routing by removing BrowserRouter and Routes components.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "./App";
-import Dashboard from "./Dashboard";
+import AppRoutes from "./routes/AppRoutes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>,
 );
