ebee6b8d2ae279f26df9c932165d90c40dac36eb
Chappie Projects <projetocolab53@gmail.com>
2026-06-16 10:06:38 -0300

    Update route imports and paths in index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 import usersRouter from "./routes/users.routes.js";
 import teamsRouter from "./routes/teams.routes.js"
-import categoriesRouter from "./routes/categories.router.js"
+import categoriesRouter from "./routes/categories.routes.js"
 import skillsRouter from "./routes/skills.routes.js";
 import projectsRoutes from "./routes/projects.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import userSkillsRoutes from "./routes/skills-users.routes.js"
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use("/usuarios", usersRouter);
 app.use("/equipes", teamsRouter);
 app.use("/categorias", categoriesRouter);
 app.use("/skills", skillsRouter);
-app.use("/projects", projectsRoutes);
+app.use("/projetos", projectsRoutes);
+app.use("/auth", authRoutes);
+app.use("/user-skill", userSkillsRoutes);
 
 app.listen(3000);
