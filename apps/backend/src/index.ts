d9001e93fd8ffd4abf8c34c9516c4013f0476dc8
Lucas <lucasemannuelll14@gmail.com>
2026-06-18 13:53:05 -0300

    refactor: renomeia rotas e importações para português

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,25 +1,25 @@
 import express from "express";
 
-import usersRouter from "./routes/users.routes.js";
-import teamsRouter from "./routes/teams.routes.js"
-import categoriesRouter from "./routes/categories.routes.js"
+import usuariosRouter from "./routes/usuarios.routes.js";
+import equipesRouter from "./routes/equipes.routes.js"
+import categoriasRouter from "./routes/categorias.routes.js"
 import skillsRouter from "./routes/skills.routes.js";
-import projectsRoutes from "./routes/projects.routes.js";
-import authRoutes from "./routes/auth.routes.js";
-import userSkillsRoutes from "./routes/skills-user.routes.js"
-import projectSkillRoutes from "./routes/skills-projects.routes.js"
+import projetosRouter from "./routes/projetos.routes.js";
+import autenticacaoRouter from "./routes/autenticao.routes.js";
+import skillsUsuariosRouter from "./routes/skills-usuarios.routes.js"
+import skillsProjetosRouter from "./routes/skills-projetos.routes.js"
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/usuarios", usersRouter);
-app.use("/equipes", teamsRouter);
-app.use("/categorias", categoriesRouter);
+app.use("/usuarios", usuariosRouter);
+app.use("/equipes", equipesRouter);
+app.use("/categorias", categoriasRouter);
 app.use("/skills", skillsRouter);
-app.use("/projetos", projectsRoutes);
-app.use("/auth", authRoutes);
-app.use("/user-skill", userSkillsRoutes);
-app.use("/project-skills", projectSkillRoutes);
+app.use("/projetos", projetosRouter);
+app.use("/autenticacao", autenticacaoRouter);
+app.use("/skills-usuarios", skillsUsuariosRouter);
+app.use("/skills-projetos", skillsProjetosRouter);
 
 app.listen(3000);
